0c82a9ee75ebec13fc6a52421a8996c1c83be264
RicardoMorenoCantea <ricardomorenocantea13@gmail.com>
2022-12-12 13:16:23 +0100

    Initial commit

---
+++ src/Abstraccion/Animal.java
@@ -0,0 +1,8 @@
+package Abstraccion;
+
+abstract class ClaseAbstracta {
+    // Las clases abstractas son aquellas que no se pueden instanciar
+    // Es decir, no es posible crear objetos de estas.
+    abstract void metodoAbstracto();
+
+}
---
+++ src/Abstraccion/Gato.java
@@ -0,0 +1,2 @@
+package Abstraccion;public class Gato {
+}
