333255a7a1cd93f6342cd36968f79066936fb782
RickyCT13 <115982761+RickyCT13@users.noreply.github.com>
2023-01-11 18:52:32 +0100

    Commit acts

---
+++ Act33/Principal.java
@@ -0,0 +1,12 @@
+package Act33;
+
+public class Principal {
+    public static void main(String[] args) {
+        Abuela c1 = new Abuela();
+        Madre c2 = new Madre();
+        Hija c3 = new Hija();
+        c1.suma(21, 23);
+        c2.year();
+        c3.esElCieloAzul();
+    }
+}
