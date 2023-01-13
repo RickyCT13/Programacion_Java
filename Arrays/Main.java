d4cb494b387bd1eb8d4ef774d6c63bee88b50b88
RickyCT13 <115982761+RickyCT13@users.noreply.github.com>
2023-01-13 14:58:31 +0100

    a

---
+++ Arrays/Main.java
@@ -0,0 +1,11 @@
+package Arrays;
+public class Main {
+    public static void main(String[] args) {
+        // Crear e imprimir por consola un array de los 5 primeros numeros enteros
+        Integer[] enteros = {1, 2, 3, 4, 5};//Integer[] enteros = new Integer[5];
+        for (Integer i = 0, j = enteros.length; i < j; i++) {
+            System.out.printf("%d ", enteros[i]);
+        }
+
+    }
+}
