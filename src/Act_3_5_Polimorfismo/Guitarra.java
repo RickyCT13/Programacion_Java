b1b410bbeaa50658382b99e482dcf74ecd99fd7b
RickyCT13 <115982761+RickyCT13@users.noreply.github.com>
2023-01-15 12:02:41 +0100

    Add files via upload

---
+++ src/Act_3_5_Polimorfismo/Guitarra.java
@@ -0,0 +1,8 @@
+package Polimorfclase;
+
+public class Guitarra extends Instrumento {
+    @Override
+    public void tocar() {
+        System.out.println("*Sonido de guitarra*");
+    }
+}
---
+++ src/Act_3_5_Polimorfismo/Tambor.java
@@ -0,0 +1,8 @@
+package Polimorfclase;
+
+public class Tambor extends Instrumento {
+    @Override
+    public void tocar() {
+        System.out.println("*Sonido de tambor*");
+    }
+}
