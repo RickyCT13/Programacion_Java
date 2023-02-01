398ebf5ce032522f55d78d1e3299d5e37275b029
RickyCT13 <115982761+RickyCT13@users.noreply.github.com>
2023-02-01 09:27:14 +0100

    Acts UD4

---
+++ ActsUD4/Act4_5/Main.java
@@ -0,0 +1,11 @@
+package ActsUD4.Act4_5;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        int[] b = new int[4];
+        for (int i = 0; i < b.length; i++) {
+
+        }
+    }
+}
