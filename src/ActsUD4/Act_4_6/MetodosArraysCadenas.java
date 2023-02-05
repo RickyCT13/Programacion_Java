cd6d2f92bc3d26d1eca6bd3daf8d21b93a8ff9a1
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-02-05 14:24:41 +0100

    Act 4.6 Terminada

--- src/ActsUD4/Act_4_6/MetodosArraysCadenas.java
+++ src/ActsUD4/Act_4_6/MetodosArraysCadenas.java
@@ -1,22 +1,8 @@
-package ActsUD4.ActivStrings;
+package ActsUD4.Act_4_6;
 
 import java.util.Arrays;
 
-public class MainStrings {
-    public static void main(String[] args) {
-        /* char[] chars = {'H', 'o', 'l', 'a'};
-        String myStr = new String(chars);
-        System.out.println(myStr);
-        findChar(myStr, 2);
-        myStr.split(".");*/
-
-        String a = "Hola";
-        findChar(a, 2);
-
-        String miTexto = "lorem ipsum dolor sit amet. consectetur adipiscing elit. nam vel.";
-        encriptar(miTexto);
-    }
-
+public class MetodosArraysCadenas {
     public static void findChar(String str, int pos) {
         if (pos <= str.length() && pos >= 0) {
             System.out.println("El caracter en la posición " + pos + " es: " + str.charAt(pos - 1));
@@ -24,11 +10,11 @@ public class MainStrings {
         else System.out.println("Posición no válida.");
     }
 
-    public static void imprimeLongitudCadena(String s) {
+    public static void printStringLength(String s) {
         System.out.println("La longitud de " + s + " es " + s.length());
     }
 
-    public static void encontrarOcurrencias(String s, char target) {
+    public static void findCharOccurrences(String s, char target) {
         int contador = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == target) {
@@ -39,11 +25,13 @@ public class MainStrings {
     }
 
 
-    public void imprimirCadenasEnOrdenAlfabetico(String[] cadenas) {
 
+    public static void printStringsInAlphabeticalOrder(String[] cadenas) {
+        Arrays.sort(cadenas);
+        System.out.println(Arrays.toString(cadenas));
     }
 
-    public static void encriptar(String textoAEncriptar) {
+    public static void encrypt(String textoAEncriptar) {
         // split por "." y reemplazar letras por nums equiv.
         char[] letrasNorm = {'a', 'b', 'e', 'g', 'i', 'o', 's', 't', 'z'};
         char[] letrasEnc = {'4', '6', '3', '9', '1', '0', '5', '7', '2'};
@@ -60,9 +48,15 @@ public class MainStrings {
         for (int i = 0; i < textoFinal.split("\\.").length; i++) {
             System.out.println(textoFinal.split("\\.")[i]);
         }
-
-        /*for (int i = 0; i < textoEncDiv.length; i++) {
-            System.out.println(textoEncDiv[i]);
-        }*/
     }
+
+    public static String[] intToString (int[] numArray) {
+        Arrays.sort(numArray);
+        String[] strArray = new String[numArray.length];
+        for (int i = 0; i < numArray.length; i++) {
+            strArray[i] = String.valueOf(numArray[i]);
+        }
+        return strArray;
+    }
+
 }
